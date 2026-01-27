db92161cff8cadde3de563fc0cc50fd61fca97d3
ElArquitectorgo <mega2814s@gmail.com>
2026-01-27 17:10:00 +0100

    Implemented regex pattern

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import regex as re
 
 def get_pair_counts(ids: list):
     """
@@ -39,15 +40,15 @@ def merge(tokens: list, pair: tuple, idx: int):
 
     return new_ids
 
-def byte_pair_encoding(tokens: list, vocab_size: int=256):
+def byte_pair_encoding(ids: list, vocab_size: int=256):
     """
     Apply the Byte Pair Encoding algorithm to iteratively merge 
     the most common pairs to create new tokens.
     Returns a dictionary with the merges produced.
     Example: bpe([1,2,9,9,1,2], 258) -> {(1, 2): 256, (256, 9): 257}
     
-    :param tokens: List of integers.
-    :type tokens: list
+    :param ids: List of integers.
+    :type ids: list
     :param vocab_size: This number determines the number of passes BPE is going to do.
     The default value if 256, meaning no passes will be executed. Since we already
     have a default dictionary of 256 tokens (utf-8), vocab_size needs to be greater
@@ -58,11 +59,15 @@ def byte_pair_encoding(tokens: list, vocab_size: int=256):
     passes = vocab_size - 256
     merges = {}
     for i in range(passes):
-        c = get_pair_counts(tokens)
+        stats = []
+        for chunk_ids in ids:
+            c = get_pair_counts(chunk_ids).most_common(1)
+            if c:
+                stats.append(c[0])
+        pair = max(stats, key=lambda x: x[1])[0]
         idx = 256 + i
-        pair = c.most_common(1)[0][0]
         merges[pair] = idx
-        tokens = merge(tokens, pair, idx)
+        ids = [merge(chunk_ids, pair, idx) for chunk_ids in ids]
 
     return merges
 
@@ -73,6 +78,9 @@ class Tokenizer:
     def __init__(self):
         self.merges = {}
         self.vocab = {}
+        # https://github.com/openai/tiktoken/blob/main/tiktoken_ext/openai_public.py -> gpt4 split pattern
+        self.pattern = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+        self.compiled_pattern = re.compile(self.pattern)
 
     def train(self, text: str, vocab_size: int=276):
         """
@@ -85,23 +93,19 @@ class Tokenizer:
         :param vocab_size: The desired final vocabulary size.
         :type vocab_size: int
         """
-        tokens = text.encode("utf-8") 
-        ids = list(tokens)
+        text_chunks = re.findall(self.compiled_pattern, text)
+        ids = [list(ch.encode("utf-8")) for ch in text_chunks]
         merges = byte_pair_encoding(ids, vocab_size)
         vocab = {idx: bytes([idx]) for idx in range(256)}
         for pair, idx in merges.items():
             vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
 
-        print("tokens length:", len(tokens))
-        print("ids length:", len(ids))
-        print(f"compression ratio: {len(tokens) / len(ids):.2f}")
-
         self.merges = merges
         self.vocab = vocab
 
-    def encode(self, text: str):
+    def encode_chunk(self, text: str):
         """
-        Returns a list of integers with the codified text sequence.
+        Codifies text into a list of integers.
         
         :param text: Text to encode.
         :type text: str
@@ -114,8 +118,24 @@ class Tokenizer:
                 break
             idx = self.merges[pair]
             tokens = merge(tokens, pair, idx)
-
         return tokens
+
+    def encode(self, text: str):
+        """
+        Codifies text into a list of integers. The text is firts splitted into
+        chunks with the regex pattern. Then we encoded those chunks separately,
+        and join the results in ids.
+        
+        :param text: Text to encode.
+        :type text: str
+        """
+        text_chunks = re.findall(self.compiled_pattern, text)
+        ids = []
+        for chunk in text_chunks:
+            chunk_ids = self.encode_chunk(chunk)
+            ids.extend(chunk_ids)
+
+        return ids
 
     def decode(self, ids: list):
         """
