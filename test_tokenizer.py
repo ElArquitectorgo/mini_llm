b9887be7e57fdfb4d7245507cdc0db22c8ec6d49
ElArquitectorgo <mega2814s@gmail.com>
2026-01-27 12:58:55 +0100

    Added documentation

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -1,9 +1,20 @@
+from collections import Counter
 import pytest
-from tokenizer import Tokenizer
+from tokenizer import Tokenizer, get_pair_counts, merge
 
 train_text = """In Chapter 2, we looked at machine learning models that treat pixels as being inde‐pendent inputs. Traditional fully connected neural network layers perform poorly onimages because they do not take advantage of the fact that adjacent pixels are highlycorrelated (see Figure 3-1). Moreover, fully connecting multiple layers does not makeany special provisions for the 2D hierarchical nature of images. Pixels close to eachother work together to create shapes (such as lines and arcs), and these shapes them‐selves work together to create recognizable parts of an object (such as the stem andpetals of a flower)."""
 val_text = """The deep neural network that we developed in Chapter 2 had two hidden layers, onewith 64 nodes and the other with 16 nodes. One way to think about this networkarchitecture is shown in Figure 3-2. In some sense, all the information contained inthe input image is being represented by the penultimate layer, whose output consistsof 16 numbers. These 16 numbers that provide a representation of the image arecalled an embedding. Of course, earlier layers also capture information from the inputimage, but those are typically not used as embeddings because they are missing someof the hierarchical information."""
 
+def test_counter():
+    c = get_pair_counts([1,2,9,9,1,2])
+    expected = Counter({(1, 2): 2, (2, 9): 1, (9, 9): 1, (9, 1): 1})
+    assert c == expected
+
+def test_merge():
+    l = merge([1,2,9,9,1,2], (1,2), 256)
+    expected = [256, 9, 9, 256]
+    assert l == expected
+    
 def test_encode_decode_train():
     tokenizer = Tokenizer()
     tokenizer.train(train_text)
--- tokenizer.py
+++ tokenizer.py
@@ -1,13 +1,31 @@
 from collections import Counter
 
 def get_pair_counts(ids: list):
+    """
+    Return a counter object that contains the frequency of every consecutive pair of elements.
+    Example: [1,2,9,9,1,2] -> Counter({(1, 2): 2, (2, 9): 1, (9, 9): 1, (9, 1): 1})
+
+    :param ids: List of integers.
+    :type ids: list
+    """
     c = Counter()
-    for i in range(0, len(ids) - 2):
+    for i in range(0, len(ids) - 1):
         c[(ids[i], ids[i+1])] += 1 
 
     return c
 
 def merge(tokens: list, pair: tuple, idx: int):
+    """
+    Replace all occurrences of pair in tokens with the new integer idx.
+    Example: merge([1,2,9,9,1,2], (1,2), 256) -> [256, 9, 9, 256]
+    
+    :param tokens: List of integers.
+    :type tokens: list
+    :param pair: The pair that will be replaced.
+    :type pair: tuple
+    :param idx: The number that will replace the pair.
+    :type idx: int
+    """
     new_ids = []
     k = 0
     while k < len(tokens):
@@ -21,7 +39,22 @@ def merge(tokens: list, pair: tuple, idx: int):
 
     return new_ids
 
-def byte_pair_encoding(tokens: list, vocab_size):
+def byte_pair_encoding(tokens: list, vocab_size: int=256):
+    """
+    Apply the Byte Pair Encoding algorithm to iteratively merge 
+    the most common pairs to create new tokens.
+    Returns a dictionary with the merges produced.
+    Example: bpe([1,2,9,9,1,2], 258) -> {(1, 2): 256, (256, 9): 257}
+    
+    :param tokens: List of integers.
+    :type tokens: list
+    :param vocab_size: This number determines the number of passes BPE is going to do.
+    The default value if 256, meaning no passes will be executed. Since we already
+    have a default dictionary of 256 tokens (utf-8), vocab_size needs to be greater
+    in order to reduce the size of tokens.
+    :type vocab_size: int
+    """
+    assert vocab_size >= 256
     passes = vocab_size - 256
     merges = {}
     for i in range(passes):
@@ -34,11 +67,24 @@ def byte_pair_encoding(tokens: list, vocab_size):
     return merges
 
 class Tokenizer:
+    """
+    This class provides core functionality for training a tokenizer and encoding/decoding text.
+    """
     def __init__(self):
         self.merges = {}
         self.vocab = {}
 
     def train(self, text: str, vocab_size: int=276):
+        """
+        Trains the Tokenizer from a text string. Generates a merges dictionary
+        to encode future text strings and a vocabulary dictionary to decode
+        previously encoded text.
+        
+        :param text: A string that will be used to train the Tokenizer.
+        :type text: str
+        :param vocab_size: The desired final vocabulary size.
+        :type vocab_size: int
+        """
         tokens = text.encode("utf-8") 
         ids = list(tokens)
         merges = byte_pair_encoding(ids, vocab_size)
@@ -54,6 +100,12 @@ class Tokenizer:
         self.vocab = vocab
 
     def encode(self, text: str):
+        """
+        Returns a list of integers with the codified text sequence.
+        
+        :param text: Text to encode.
+        :type text: str
+        """
         tokens = list(text.encode("utf-8"))
         while len(tokens) > 1:
             c = get_pair_counts(tokens)
@@ -66,6 +118,12 @@ class Tokenizer:
         return tokens
 
     def decode(self, ids: list):
+        """
+        Returns the real string codified in ids.
+        
+        :param ids: List of integers.
+        :type ids: list
+        """
         tokens = b"".join(self.vocab[idx] for idx in ids)
         text = tokens.decode("utf-8", errors="replace")
         return text
